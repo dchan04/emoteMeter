44f1d14fa21d90b5e6b5f074857964f68285a669
dchan04 <47226733+dchan04@users.noreply.github.com>
2022-12-03 14:05:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const channel = ${{secrets.TARGET_CHANNEL}};
+const channel = "stryander";
 const countElement = document.getElementById("count");
 const gauge = document.querySelector(".gaugeBar");
 const emoteImg = document.getElementById("emote");
