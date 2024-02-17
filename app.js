6217acf1adecb7f9219c047b9dee8994c30fe0be
dchan04 <darrenchan1123@gmail.com>
2024-02-17 13:07:48 -0500

    Added more emotes & goal counter
    Added more emotes. Added a new mario themed goal counter. Improved code by using maps instead of an awfully long switch statement.

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const channel = "strylander";
 const countElement = document.getElementById("count");
+const goalElement = document.getElementById("goal-count");
 const gauge = document.querySelector(".gaugeBar");
 const emoteImg = document.getElementById("emote");
+const emoteMap = new Map([
+	["gigl", "gigl.png"],
+	["GOTTEM", "GOTTEM.png"],
+	["HEHE", "HEHE.png"],
+	["icent", "icent.png"],
+	["KEKWPoint", "KEKWPoint.png"],
+	["laff", "laff.png"],
+	["LOLW", "LOLW.png"],
+	["mokokoGiggle", "mokokoGiggle.png"],
+	["OMEGALOOL", "OMEGALOOL.png"],
+	["OMEGALUL", "OMEGALUL.png"],
+	["LUL", "LUL.png"],
+	["pepeLaugh", "pepeLaugh.png"],
+	["PepeLmao", "PepeLmao.png"],
+	["pepePoint", "pepePoint.png"],
+	["xddICANT", "xddICANT.png"],
+	["xdding", "xdding.png"],
+	["ICANT", "ICANT.png"],
+	["KEKW", "KEKW.png"],
+	["xdd", "xdd.png"],
+	["LUL", "LUL.png"],
+	["om", "om.png"],
+]);
 
 var step = 0,
 	count = 0,
+	goalCount = 0,
 	res;
 
 var timer = setInterval(loop, 100);
@@ -50,176 +75,36 @@ window.onload = () => {
 	});
 
 	client.on("message", (channel, tags, message, self) => {
-		if (
-			message.includes("KEKW") ||
-			message.includes("pepeLaugh") ||
-			message.includes("LUL") ||
-			message.includes("GOTTEM") ||
-			message.includes("LOLW") ||
-			message.includes("PepePoint") ||
-			message.includes("PepeLmao") ||
-			message.includes("ICANT") ||
-			message.includes("OMEGALUL") ||
-			message.includes("laff") ||
-			message.includes("xdd") ||
-			message.includes("mokokoGiggle") ||
-			message.includes("gigl") ||
-			message.includes("KEKWPoint") ||
-			message.includes("icent") ||
-			message.includes("xddICANT") ||
-			message.includes("xdding")
-		) {
-			switch (true) {
-				case message.includes("KEKWPoint"):
-					if (!emoteImg.src.includes("KEKWPoint.png")) {
-						emoteImg.src = "./img/KEKWPoint.png";
+		for (const [keyword, emoteFileName] of emoteMap) {
+			if (message.includes(keyword)) {
+				const emotePath = emoteFileName
+					? `./img/${emoteFileName}`
+					: null;
+
+				if (emotePath && !emoteImg.src.includes(emotePath)) {
+					emoteImg.src = emotePath;
+					emoteImg.classList.remove("swing-in-top-fwd");
+					emoteImg.offsetWidth;
+					emoteImg.classList.add("swing-in-top-fwd");
+					count = parseInt(countElement.textContent) + 20;
+					if (count > 100) {
+						emoteImg.classList.remove("bounce-7");
 						emoteImg.classList.remove("swing-in-top-fwd");
 						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
+						emoteImg.classList.add("bounce-7");
+						count = 0;
+						goalCount += 1;
 					}
-					break;
-				case message.includes("KEKW"):
-					if (!emoteImg.src.includes("KEKW.png")) {
-						emoteImg.src = "./img/KEKW.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("pepeLaugh"):
-					if (!emoteImg.src.includes("pepeLaugh.png")) {
-						emoteImg.src = "./img/pepeLaugh.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("OMEGALUL"):
-					if (!emoteImg.src.includes("OMEGALUL.png")) {
-						emoteImg.src = "./img/OMEGALUL.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("xddICANT"):
-					if (!emoteImg.src.includes("xddICANT.png")) {
-						emoteImg.src = "./img/xddICANT.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("xdding"):
-					if (!emoteImg.src.includes("xdding.png")) {
-						emoteImg.src = "./img/xdding.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("xdd"):
-					if (!emoteImg.src.includes("xdd.png")) {
-						emoteImg.src = "./img/xdd.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("GOTTEM"):
-					if (!emoteImg.src.includes("GOTTEM.png")) {
-						emoteImg.src = "./img/GOTTEM.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("LOLW"):
-					if (!emoteImg.src.includes("LOLW.png")) {
-						emoteImg.src = "./img/LOLW.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("PepePoint"):
-					if (!emoteImg.src.includes("pepePoint.png")) {
-						emoteImg.src = "./img/pepePoint.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("PepeLmao"):
-					if (!emoteImg.src.includes("PepeLmao.png")) {
-						emoteImg.src = "./img/PepeLmao.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("ICANT"):
-					if (!emoteImg.src.includes("ICANT.png")) {
-						emoteImg.src = "./img/ICANT.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("LUL"):
-					if (!emoteImg.src.includes("LUL.png")) {
-						emoteImg.src = "./img/LUL.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("laff"):
-					if (!emoteImg.src.includes("laff.png")) {
-						emoteImg.src = "./img/laff.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("gigl"):
-					if (!emoteImg.src.includes("gigl.png")) {
-						emoteImg.src = "./img/gigl.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("mokokoGiggle"):
-					if (!emoteImg.src.includes("mokokoGiggle.png")) {
-						emoteImg.src = "./img/mokokoGiggle.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				case message.includes("icent"):
-					if (!emoteImg.src.includes("icent.png")) {
-						emoteImg.src = "./img/icent.png";
-						emoteImg.classList.remove("swing-in-top-fwd");
-						emoteImg.offsetWidth;
-						emoteImg.classList.add("swing-in-top-fwd");
-					}
-					break;
-				default:
+				}
+
+				break; // Stop checking once a matching keyword is found
 			}
-			count = parseInt(countElement.textContent) + 20;
-			if (count > 100) {
-				emoteImg.classList.remove("bounce-7");
-				emoteImg.classList.remove("swing-in-top-fwd");
-				emoteImg.offsetWidth;
-				emoteImg.classList.add("bounce-7");
-				count = 0;
-			}
-			countElement.innerHTML = count;
-			gauge.style.transform = `rotate(${count / 2 / 100}turn)`;
-			gauge.style.transition = `transform 0.2s ease-in-out`;
 		}
+
+		countElement.innerHTML = count;
+		goalElement.innerHTML = goalCount;
+		gauge.style.transform = `rotate(${count / 2 / 100}turn)`;
+		gauge.style.transition = `transform 0.2s ease-in-out`;
 	}); //count number of emotes then
 };
 
